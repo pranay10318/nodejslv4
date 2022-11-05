f5af96572ffeee40956bc903d68a2bce6f97d2bb
pranay10318 <109957442+pranay10318@users.noreply.github.com>
2022-11-05 22:01:07 +0530

    Initial commit

---
+++ todo.js
@@ -0,0 +1,60 @@
+const todoList = () => {
+  all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
+
+  const overdue = () => {
+    return all.filter((todos) => todos.dueDate == yesterday);
+  };
+
+  const dueToday = () => {
+    // Write the date check condition here and return the array of todo items that are due today accordingly.
+    // FILL YOUR CODE HERE
+    // ..
+    // ..
+    // ..
+    return all.filter((todos) => todos.dueDate == today);
+  };
+
+  const dueLater = () => {
+    // Write the date check condition here and return the array of todo items that are due later accordingly.
+    // FILL YOUR CODE HERE
+    // ..
+    // ..
+    // ..
+    return all.filter((todos) => todos.dueDate == tomorrow);
+  };
+
+  const toDisplayableList = (list) => {
+    // Format the To-Do list here, and return the output string as per the format given above.
+    // FILL YOUR CODE HERE
+    // ..
+    // ..
+    // ..
+    // return OUTPUT_STRING
+    return list
+      .map(
+        (todos) =>
+          `${todos.completed ? `[x]` : `[ ]`} ${todos.title} ${
+            todos.dueDate == today ? " " : todos.dueDate
+          }`
+      )
+      .join("\n");
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+
+module.exports =todoList;
