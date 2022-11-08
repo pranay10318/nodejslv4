2b3be990e70e36a7ef2616585c840ef337b02071
pranay10318 <109957442+pranay10318@users.noreply.github.com>
2022-11-08 14:07:34 +0530

    add lint staged

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,31 +1,31 @@
-const todoList=require('../todo');
-const {all,markAsComplete,add}=todoList();
-describe("test suite for todolist",()=>{
+/* eslint-disable no-undef */
+const todoList = require("../todo");
 
-    beforeAll(()=>{
-        add({
-            title:"test todo",
-            completed:false,
-            dueDate:new Date().toLocaleString("en-CA"),
-        });
+const { all, markAsComplete, add } = todoList();
+
+describe("Todolist test suite", () => {
+  beforeAll(() => {
+    add({
+      title: "test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
     });
+  });
 
-    test("should add new todo",()=>{
+  test("should add a new todo", () => {
+    const todoItemsCount = all.length;
 
-        const toDoItemsCount=all.length;
-        
-        add({
-            title:"Test todo",
-            completed:false,
-            dueDate:new Date().toLocaleDateString("en-CA"),
-        });
-        expect(all.length).toBe( toDoItemsCount +1);
+    add({
+      title: "test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
     });
+    expect(all.length).toBe(todoItemsCount + 1);
+  });
 
-    test("should mark to do as completed",()=>{
-        expect(all[0].completed).toBe(false);
-        markAsComplete(0);
-        expect(all[0].completed).toBe(true);
-    })
-
-})
+  test("should mark a todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+});
